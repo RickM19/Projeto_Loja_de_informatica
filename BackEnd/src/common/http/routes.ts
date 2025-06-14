8ab0169db6436ecdaa18a0be58106d558d01a651
Kaio Silva <kaiofernando05cont@gmail.com>
2025-06-14 15:04:04 -0300

    feat: add User routes

--- BackEnd/src/common/http/routes.ts
+++ BackEnd/src/common/http/routes.ts
@@ -1,4 +1,6 @@
+import { UserController } from '@/controllers/UserController';
 import { Request, Response, Router } from 'express';
+import { authMiddleware } from './middlewares/authMiddleware';
 
 const routes = Router();
 
@@ -6,5 +8,11 @@ routes.get('/', (_req: Request, res: Response): void => {
     res.status(200).json({ message: 'Ola mundo!' });
     return;
 });
+
+routes.post('/user', new UserController().create);
+routes.post('/login', new UserController().login);
+
+routes.use(authMiddleware);
+routes.get('/profile', new UserController().getProfile);
 
 export { routes };
