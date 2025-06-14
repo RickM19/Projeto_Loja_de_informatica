41bd72ad7fd714bf7e87bcaf918fde059960fe34
Kaio Silva <kaiofernando05cont@gmail.com>
2025-06-14 15:02:54 -0300

    feat: create authentication middleware

---
+++ BackEnd/src/common/http/middlewares/authMiddleware.ts
@@ -0,0 +1,42 @@
+import { AppError, BadRequestError, UnauthorizedError } from "@/common/errors/AppError";
+import { userRepository } from "@/repositories/userRepository";
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { env } from "process";
+
+type JwtPayload = {
+    id: string;
+}
+
+export const authMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+    const {authorization} = req.headers;
+    
+            if(!authorization) {
+                throw new UnauthorizedError('Não autorizado');
+            }
+    
+            const token = authorization.split(' ')[1];
+    
+            let id: string;
+
+            try {
+                const payload = jwt.verify(token, env.JWT_PASS ?? '') as JwtPayload;
+                id = payload.id;
+            } catch(err) {
+                throw new BadRequestError("Token inválido");
+            }
+    
+            const user = await userRepository.findOneBy({id});
+    
+            if(!user) {
+                throw new UnauthorizedError("Não autorizado");
+            }
+    
+            const {password: _, ...loggedUser} = user;
+    
+            res.json(loggedUser);
+            
+            req.user = loggedUser;
+
+            next();
+}
