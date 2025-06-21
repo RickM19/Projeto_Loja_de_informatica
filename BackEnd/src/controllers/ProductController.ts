0fc3ce60c2fa7a3f4c5677c577d5b999ecce4abe
Kaio Silva <kaiofernando05cont@gmail.com>
2025-06-21 10:22:51 -0300

    feat: create Product controller

---
+++ BackEnd/src/controllers/ProductController.ts
@@ -0,0 +1,38 @@
+import { BadRequestError } from "@/common/errors/AppError";
+import { productRepository } from "@/repositories/productRepository";
+import { ProductService } from "@/services/ProductService";
+import { Request, Response } from "express";
+
+export class ProductController {
+    
+    private service = new ProductService()
+
+    async create(req: Request, res: Response) {
+        await this.service.create(req.body)
+
+        res.status(201).send()
+    }
+
+    async getById(req: Request, res: Response) {
+        const product = await this.service.getById(req.params?.id);
+        res.status(200).json(product);
+    }
+
+    async getFiltered(req: Request, res: Response) {
+        const products = await this.service.getByParams(req.query);
+        res.status(200).json(products);
+    }
+
+    async update(req: Request, res: Response) {
+        await this.service.update(req.params?.id, req.body);
+
+        res.status(204).send();
+    }
+
+    async delete(req: Request, res: Response) {
+        await this.service.delete(req.params?.id);
+
+        res.status(204).send();
+    }
+
+}
