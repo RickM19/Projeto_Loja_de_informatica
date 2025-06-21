088882ae7907871d739082da7b8ec6fb18fa1cb5
Kaio Silva <kaiofernando05cont@gmail.com>
2025-06-21 10:23:11 -0300

    feat: create Product routes

--- BackEnd/src/common/http/routes.ts
+++ BackEnd/src/common/http/routes.ts
@@ -1,3 +1,4 @@
+import { ProductController } from '@/controllers/ProductController';
 import { Request, Response, Router } from 'express';
 
 const routes = Router();
@@ -6,5 +7,12 @@ routes.get('/', (_req: Request, res: Response): void => {
     res.status(200).json({ message: 'Ola mundo!' });
     return;
 });
+
+const productController = new ProductController();
+routes.post('/product', async(req, res) => productController.create(req, res));
+routes.get('/product/:id', async (req, res) => productController.getById(req, res));
+routes.get('/product', async (req, res) => productController.getFiltered(req, res));
+routes.put('/product/:id', async (req, res) => productController.update(req, res));
+routes.delete('/product/:id', async (req, res) => productController.delete(req, res));
 
 export { routes };
