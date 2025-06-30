08e839f312188e597b2bc1f84c6f3092b62aee31
RickM19 <riquelmo14br@gmail.com>
2025-06-29 21:02:22 -0300

    Fix: fix customer repository name

--- BackEnd/src/repositories/CustomerRepository.ts
+++ BackEnd/src/repositories/CustomerRepository.ts
@@ -2,13 +2,9 @@ import { DataSource } from 'typeorm';
 import { Customer } from '@/entities/Customer';
 import { Repository } from 'typeorm';
 
-export class UserRepository extends Repository<Customer> {
+export class CustomerRepository extends Repository<Customer> {
     constructor(dataSource: DataSource) {
         super(Customer, dataSource.manager);
-    }
-
-    async findByEmail(email: string) {
-        return this.findOneBy({ email });
     }
     async findByCpf(cpf: string) {
         return this.findOneBy({ cpf });
