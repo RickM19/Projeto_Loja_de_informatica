cbb7756c64b05afa0cb6861a34d6ff93ee79ee69
Kaio Silva <kaiofernando05cont@gmail.com>
2025-07-11 21:58:50 -0300

    fix: redirect to login page when token is not valid

--- FrontEnd/src/utils/protectedRoutes.tsx
+++ FrontEnd/src/utils/protectedRoutes.tsx
@@ -1,6 +1,27 @@
+import axios from "@/api/axios";
+import { useEffect } from "react";
 import { Outlet, Navigate } from "react-router-dom";
+const PROFILE_URL = "/profile"
 
 export const ProtectedRoutes = () => {
-    const user = localStorage.getItem("accessToken");
-    return user ? <Outlet /> : <Navigate to={"/login"}/>
+    const token = localStorage.getItem("accessToken");
+    useEffect(() => {
+        const verifyToken = async () => {
+            try {
+                await axios.get(PROFILE_URL, {
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${token}`
+                    },
+                    withCredentials: true
+                });
+                console.log("Aprovado");
+            } catch (error) {
+                localStorage.clear();
+                console.log(error);
+            }
+        };
+        verifyToken();
+    }, []);
+    return token ? <Outlet /> : <Navigate to={"/login"}/>
 }
