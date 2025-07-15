581430744b153110adc0a28248bb897bbaf22be8
RickM19 <riquelmo14br@gmail.com>
2025-07-15 14:59:26 -0300

    fix: fix customer route

--- BackEnd/src/routes/CustomerRoutes.ts
+++ BackEnd/src/routes/CustomerRoutes.ts
@@ -5,11 +5,11 @@ const customerRoutes = Router();
 const customerController = new CustomerController();
 
 customerRoutes.post('/', customerController.create);
+customerRoutes.get('/old', customerController.getOldCustomers);
 customerRoutes.get('/:id', customerController.getById);
 customerRoutes.get('/', customerController.getFiltered);
 customerRoutes.put('/:id', customerController.update);
 customerRoutes.delete('/:id', customerController.delete);
 customerRoutes.get('/cpf/:cpf', customerController.getbyCpf);
-customerRoutes.get('/old', customerController.getOldCustomers);
 
 export { customerRoutes };
